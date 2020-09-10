b9fdf8ef240e124d63e58ced8f495637fc78714b
JossWritesCode <joscelynrose@gmail.com>
2020-09-09 23:35:17 -0400

    solved the first two problems

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,18 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // save an output string
+  let output = "";
+  // loop over the string backward
+  for (let i = str.length - 1; i >= 0; i--) {
+    //add each char to output
+    output += str[i];
+  }
+  console.log(output);
+  return output;
+
+  // return output
+}
 
 module.exports = reverse;
