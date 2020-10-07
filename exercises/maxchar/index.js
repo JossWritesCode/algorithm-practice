4c2ddd56b1d6c0b1f7848ec5a2d32a0f1cc62657
JossWritesCode <joscelynrose@gmail.com>
2020-10-07 19:44:42 -0400

    completed the maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const hashMap = {};
+
+  for (let char of str) {
+    hashMap[char] = hashMap[char] + 1 || 1;
+  }
+
+  let max = 0;
+  let biggestChar;
+  for (let char in hashMap) {
+    if (hashMap[char] > max) {
+      max = hashMap[char];
+      biggestChar = char;
+    }
+  }
+  console.log(hashMap, 'hashMap');
+  return biggestChar;
+}
 
 module.exports = maxChar;
